6fa042975769b8909edb20304ed240acda6a9ac4
SpldierDetector <set.97@mail.ru>
2026-07-14 22:54:54 +1000

    show latest message in chat previews

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,9 +6,12 @@ import { styles } from '@/styles/index.styles';
 
 import { ChatPreview } from '@/components/chat-preview';
 import { chats } from '@/data/chat';
+import { useMessages } from '@/providers/messages-provider';
 
 
 export default function ChatListScreen() {
+  const { messages } = useMessages();
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Voxa</Text>
@@ -16,24 +19,31 @@ export default function ChatListScreen() {
       <FlatList
         data={chats}
         keyExtractor={(chat) => chat.id.toString()}
-        renderItem={({ item }) => (
-          <Pressable
-          style={({ pressed }) => [
-            styles.chatPreview,
-            pressed && styles.chatPreviewPressed,
-          ]}  
-          onPress={() => {
-              router.push(`/chat/${item.id}` as Href);
-            }}
-          >
-            <ChatPreview
-              name={item.name}
-              lastMessage={item.lastMessage}
-              time={item.time}
-              isOnline={item.isOnline}
-            />
-          </Pressable>
-        )}
+        renderItem={({ item }) => {
+          const chatMessages = messages.filter(
+            (message) => message.chatId === item.id
+          );
+
+          const lastMessage = chatMessages.at(-1);
+          return (
+            <Pressable
+            style={({ pressed }) => [
+              styles.chatPreview,
+              pressed && styles.chatPreviewPressed,
+            ]}  
+            onPress={() => {
+                router.push(`/chat/${item.id}` as Href);
+              }}
+            >
+              <ChatPreview
+                name={item.name}
+                lastMessage={lastMessage?.text ?? item.lastMessage}
+                time={item.time}
+                isOnline={item.isOnline}
+              />
+            </Pressable>
+          );
+        }}
       />
     </SafeAreaView>
   );
